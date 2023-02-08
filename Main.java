e2e3ea0c1dec80be18ecffc484ecc6819310c6ff
Myreen Leanca <124058246+mleanca@users.noreply.github.com>
2023-02-08 13:31:30 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(20);
+        circle.calculateArea();
+        System.out.println("Area of circle is = " + circle.calculateArea());
+    }
+}
